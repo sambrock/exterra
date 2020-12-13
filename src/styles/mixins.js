87843ed92886e0fcaf498185df7dae2ff59a8104
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2020-12-13 02:41:05 +0000

    Added new mixins.

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -31,12 +31,73 @@ const mixins = {
     line-height: 100%;
   `,
   time: css`
-    color: var(--white);
+    /* color: var(--white); */
     font-weight: 500;
   `,
-  glow: css`
-    box-shadow: 0 0 10px #fff;
-    text-shadow: 0 0 10px #fff;
+  pageContainer: css`
+    width: 800px;
+    margin: 120px auto;
+  `,
+  sectionH2: css`
+    display: flex;
+    align-items: center;
+    color: var(--white-opacity);
+    span {
+      margin: 0 0 0 12px;
+      height: 2px; 
+      width: 60px;
+      background: var(--white-opacity);
+    }
+  `,
+  hlHeader: css`
+    height: 2px;
+    width: 60px;
+    background: var(--white-opacity);
+  `,
+  label: css`
+    text-transform: uppercase;
+    font-size: var(--fz-sm);
+    font-weight: 500;
+    letter-spacing: 1.5px;
+    color: var(--white-opacity);
+  `,
+  databar: css`
+    width: 100%;
+    span {
+      height: 2px;
+      display: inline-block;
+    }
+  `,
+  flexTable: css`
+    width: 100%;
+    .tr {
+      display: flex;
+      justify-content: space-between;
+      font-size: var(--fz-sm);
+      border-bottom: 1px var(--dark-blue-2) solid;
+      padding: 12px 6px;
+
+      &:nth-child(1) {
+        border-top: 1px var(--dark-blue-2) solid;
+      }
+      .th {
+        text-transform: uppercase;
+        font-size: var(--fz-sm);
+        font-weight: 500;
+        letter-spacing: 1.5px;
+        color: var(--white-opacity);
+        min-width: 100px;
+      }
+      .td {
+        font-weight: 500;
+        letter-spacing: .5px;
+        text-align: right;
+          
+        .unit {
+          color: var(--white-opacity);
+        }
+      }
+    }
   `,
 };
 
