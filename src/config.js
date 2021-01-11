0112b81d692286970c1c966fca7a3b751019d966
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-11 14:12:24 +0000

    Added new api endpoint, updated map style and default center.

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 module.exports = {
   api: {
     // baseURL: 'https://ll.thespacedevs.com/2.1.0/launch',
-    baseURL: 'https://lldev.thespacedevs.com/2.1.0/launch',
+    // baseURL: 'https://lldev.thespacedevs.com/2.1.0',
+    baseURL: 'https://spacelaunchnow.me/api/3.5.0',
   },
 
   mapbox: {
     accessToken: 'pk.eyJ1IjoieHNhbWJyb2NrIiwiYSI6ImNraWYxaHplYzBhOTYzMHFrZGVjbmk2azYifQ.YvpDpSvqa6V1SC3EVEEG3A',
-    style: 'mapbox://styles/xsambrock/ckif7859s0lmp1ao74wa6n5c7',
+    style: 'mapbox://styles/xsambrock/ckjrql6mh4ryq1ap3p7qrdkkv',
     containerStyle: { height: '100vh', width: '100vw' },
-    center: { longitude: -101.073324, latitude: 38.685516 },
+    center: { longitude: -60.073324, latitude: 38.685516 },
     zoom: { min: 3, default: 3 }
   }
 }
