db9e028c6d78a65072dbab8c2eccb5b5fb393dc2
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-14 14:20:50 +0000

    Caching upcoming launches and launch details.

--- src/api/index.js
+++ src/api/index.js
@@ -1,33 +1,50 @@
 import axios from 'axios';
+import { DateTime } from 'luxon';
 
 import { api } from '../config';
 
-// const delay = interval => new Promise(resolve => setTimeout(resolve, interval));
+export const getUpcomingLaunches = async () => {
+  let upcoming = JSON.parse(localStorage.getItem('upcoming'))
 
-// export const getLaunchById = async (id) => {
-//   await delay(500); // Delay request due to API rate limits
+  if (!upcoming || DateTime.local().ts > upcoming.fetched) {
+    try {
+      const response = await axios.get(`${api.baseURL}/launch/upcoming`);
 
-//   try {
-//     // const response = await axios.get(`https://swapi.dev/api/people/1/`);
-//     const response = await axios.get(`${api.baseURL}/${id}`);
-//     return response.data;
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
+      response.data.fetched = DateTime.local().plus({ minutes: 15 }).ts;
+      localStorage.setItem('upcoming', JSON.stringify(response.data));
+
+      upcoming = response.data;
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return upcoming;
+}
 
 export const getLaunchById = async (id) => {
-  try {
-    const response = await axios.get(`${api.baseURL}/launch/${id}`);
-    return response.data;
-  } catch (err) {
-    console.error(err)
+  let rocket = JSON.parse(sessionStorage.getItem(`${id}`))
+
+  if (!rocket || DateTime.local().ts > rocket.fetched) {
+    try {
+      const response = await axios.get(`${api.baseURL}/launch/${id}`);
+
+      // console.log('API CALLED');
+      response.data.fetched = DateTime.local().plus({ minutes: 1 }).ts;
+      sessionStorage.setItem(`${response.data.id}`, JSON.stringify(response.data));
+
+      rocket = response.data;
+    } catch (err) {
+      console.error(err)
+    }
   }
+
+  return rocket;
 }
 
 export const getRockets = async () => {
   try {
-    const response = await axios.get(`${api.baseURL}/config/launcher/`);
+    const response = await axios.get(`${api.devURL}/config/launcher/`);
     return response.data;
   } catch (err) {
     console.error(err)
@@ -36,7 +53,7 @@ export const getRockets = async () => {
 
 export const getRocketById = async (id) => {
   try {
-    const response = await axios.get(`${api.baseURL}/config/launcher/${id}`);
+    const response = await axios.get(`${api.devURL}/config/launcher/${id}`);
     return response.data;
   } catch (err) {
     console.error(err)
--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 module.exports = {
   api: {
     // baseURL: 'https://ll.thespacedevs.com/2.1.0/launch',
-    // baseURL: 'https://lldev.thespacedevs.com/2.1.0',
     baseURL: 'https://spacelaunchnow.me/api/3.5.0',
+    devURL: 'https://lldev.thespacedevs.com/2.1.0', // dev endpoint, no throttling / limits, works fine for legacy data
   },
 
   mapbox: {
     accessToken: 'pk.eyJ1IjoieHNhbWJyb2NrIiwiYSI6ImNraWYxaHplYzBhOTYzMHFrZGVjbmk2azYifQ.YvpDpSvqa6V1SC3EVEEG3A',
     style: 'mapbox://styles/xsambrock/ckjrql6mh4ryq1ap3p7qrdkkv',
     containerStyle: { height: '100vh', width: '100vw' },
-    center: { longitude: -60.073324, latitude: 38.685516 },
-    zoom: { min: 3, default: 3 }
+    center: { longitude: -95.0364, latitude: 32.8951 },
+    zoom: { min: 3, default: 4 }
   }
 }
