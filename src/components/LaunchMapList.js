1827933a98b9dddbcc312bbf0a9444d24f711f02
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-15 19:33:19 +0000

    Updated drag offset limits.

--- src/components/LaunchMapList.js
+++ src/components/LaunchMapList.js
@@ -7,10 +7,11 @@ import { clamp } from 'lodash';
 import LaunchMapItem from './LaunchMapItem';
 
 const StyledLaunchListDiv = styled(animated.div)`
-  touch-action: none;
+  touch-action: pan-x;
+  cursor: grab;
 
-  .touch-div {
-    /* touch-action: none; */
+  &.dragging {
+    cursor: grabbing;
   }
 
   @media(max-width: 480px) {
@@ -21,13 +22,16 @@ const StyledLaunchListDiv = styled(animated.div)`
 
 const LaunchMapList = ({ launches, setCenterMap }) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [dragging, setDragging] = useState(false);
 
   const container = useRef();
 
   const [{ x }, set] = useSpring(() => ({ x: [0, 0] }));
 
-  const handleDrag = ({offset}) => {
-    const offsetX = clamp(offset[0], 0, container.offsetWidth);
+  const handleDrag = (state) => {
+    setDragging(true);
+
+    const offsetX = clamp(state.offset[0], -container.current.offsetWidth, 0 );
 
     const launchElPositions = [];
     container.current.childNodes.forEach(el => launchElPositions.push(el.offsetLeft));
@@ -39,15 +43,21 @@ const LaunchMapList = ({ launches, setCenterMap }) => {
     set({ x: [-closest, 0] });
   };
 
-  const bind = useDrag(state => handleDrag(state), { drag: { bounds: { left: -4000, right: 0 } }, axis: 'x' });
+  const bind = useDrag(state => handleDrag(state), { bounds: { left: container.current ? -container.current.offsetWidth : 0, right: 0 }, lockdirection: true, axis: 'x' });
 
   return (
-    <StyledLaunchListDiv {...bind()} ref={container} className="fixed flex items-end z-10 w-screen overflow-hidden bottom-8 left-4 sm:bottom-12 sm:left-0">
+    <StyledLaunchListDiv
+      {...bind()}
+      ref={container}
+      className={`fixed flex items-end z-10 w-screen overflow-hidden bottom-8 left-4 sm:bottom-12 sm:left-0 ${dragging ? 'dragging' : ''}`}
+      onMouseUp={() => setDragging(false)}
+    >
       {launches.map((launch, index) => (
-        <animated.div className="touch-div" style={{ transform: x.interpolate(value => `translate3d(${value}px, 0px, 0px)`) }} key={launch.id}>
+        <animated.div style={{ transform: x.interpolate(value => `translate3d(${value}px, 0px, 0px)`) }} key={launch.id}>
           <LaunchMapItem launch={launch} isActive={index === activeIndex} setActive={() => setActiveIndex(index)} setCenterMap={setCenterMap} />
         </animated.div>
       ))}
+      <div className="bg-opacity h-2 w-2 border"></div>
     </StyledLaunchListDiv>
   )
 }
