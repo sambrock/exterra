4b9c81f86a50b98302fce0316efed5fbc4a32d43
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-15 12:58:00 +0000

    Added new status colour.

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,6 +6,7 @@ const theme = {
     2: 'var(--grey)', // To be determined
     3: 'var(--green)', // Launch success
     4: 'var(--red)', // Launch failure
+    5: 'var(--grey)', // Launch on hold
     6: 'var(--blue)', // Launch in flight
     7: 'var(--red)', // Partial failure
     8: 'var(--grey)' // To be confirmed
