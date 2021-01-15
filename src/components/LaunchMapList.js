3b2c59935ac9f424d7f1b5a4af26f917cfbfa1b9
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-15 23:25:22 +0000

    Removed drag animation, now using overflow-x scroll.

--- src/components/LaunchMapList.js
+++ src/components/LaunchMapList.js
@@ -1,18 +1,10 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import { useSpring, animated } from 'react-spring';
-import { useDrag, } from 'react-use-gesture';
-import { clamp } from 'lodash';
 
 import LaunchMapItem from './LaunchMapItem';
 
-const StyledLaunchListDiv = styled(animated.div)`
+const StyledLaunchListDiv = styled.div`
   touch-action: pan-x;
-  cursor: grab;
-
-  &.dragging {
-    cursor: grabbing;
-  }
 
   @media(max-width: 480px) {
     bottom: 2rem;
@@ -22,42 +14,14 @@ const StyledLaunchListDiv = styled(animated.div)`
 
 const LaunchMapList = ({ launches, setCenterMap }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [dragging, setDragging] = useState(false);
-
-  const container = useRef();
-
-  const [{ x }, set] = useSpring(() => ({ x: [0, 0] }));
-
-  const handleDrag = (state) => {
-    setDragging(true);
-
-    const offsetX = clamp(state.offset[0], -container.current.offsetWidth, 0 );
-
-    const launchElPositions = [];
-    container.current.childNodes.forEach(el => launchElPositions.push(el.offsetLeft));
-
-    const closest = launchElPositions.reduce((a, b) => {
-      return Math.abs(b - Math.abs(offsetX)) < Math.abs(a - Math.abs(offsetX)) ? b : a;
-    });
-
-    set({ x: [-closest, 0] });
-  };
-
-  const bind = useDrag(state => handleDrag(state), { bounds: { left: container.current ? -container.current.offsetWidth : 0, right: 0 }, lockdirection: true, axis: 'x' });
 
   return (
-    <StyledLaunchListDiv
-      {...bind()}
-      ref={container}
-      className={`fixed flex items-end z-10 w-screen overflow-hidden bottom-8 left-4 sm:bottom-12 sm:left-0 ${dragging ? 'dragging' : ''}`}
-      onMouseUp={() => setDragging(false)}
-    >
+    <StyledLaunchListDiv className="fixed flex items-end z-10 w-screen overflow-x-scroll bottom-0 pb-4 sm:pb-8 left-4 sm:bottom-2 sm:left-4">
       {launches.map((launch, index) => (
-        <animated.div style={{ transform: x.interpolate(value => `translate3d(${value}px, 0px, 0px)`) }} key={launch.id}>
+        <div className="touch-div" key={launch.id}>
           <LaunchMapItem launch={launch} isActive={index === activeIndex} setActive={() => setActiveIndex(index)} setCenterMap={setCenterMap} />
-        </animated.div>
+        </div>
       ))}
-      <div className="bg-opacity h-2 w-2 border"></div>
     </StyledLaunchListDiv>
   )
 }
