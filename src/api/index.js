4f696948759e5ff477832db7512c677b59d68cee
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-15 12:52:12 +0000

    Added caching to localstorage.

--- src/api/index.js
+++ src/api/index.js
@@ -4,47 +4,67 @@ import { DateTime } from 'luxon';
 import { api } from '../config';
 
 export const getUpcomingLaunches = async () => {
-  let upcoming = JSON.parse(localStorage.getItem('upcoming'))
+  let upcoming = JSON.parse(localStorage.getItem('upcoming'));
 
   if (!upcoming || DateTime.local().ts > upcoming.fetched) {
     try {
-      const response = await axios.get(`${api.baseURL}/launch/upcoming`);
+      const response = await axios.get(`${api.baseURL}/launch/upcoming/?limit=10`);
 
-      response.data.fetched = DateTime.local().plus({ minutes: 15 }).ts;
+      response.data.fetched = DateTime.local().plus({ hours: 1 }).ts;
       localStorage.setItem('upcoming', JSON.stringify(response.data));
 
       upcoming = response.data;
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
 
   return upcoming;
 }
 
+export const getPreviousLaunches = async (offset) => {
+  const url = `/launch/previous/?limit=50&offset=${offset}`;
+  let previous = JSON.parse(localStorage.getItem(url));
+
+  if (!previous || DateTime.local().ts > previous.fetched) {
+    try {
+      const response = await axios.get(api.devURL + url);
+
+      response.data.fetched = DateTime.local().plus({ hours: 1 }).ts;
+      localStorage.setItem(url, JSON.stringify(response.data));
+
+      previous = response.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return previous;
+}
+
 export const getLaunchById = async (id) => {
-  let rocket = JSON.parse(sessionStorage.getItem(`${id}`))
+  const url = `/launch/${id}`;
+  let rocket = JSON.parse(localStorage.getItem(url));
 
   if (!rocket || DateTime.local().ts > rocket.fetched) {
     try {
-      const response = await axios.get(`${api.baseURL}/launch/${id}`);
+      const response = await axios.get(api.baseURL + url);
 
-      // console.log('API CALLED');
-      response.data.fetched = DateTime.local().plus({ minutes: 1 }).ts;
-      sessionStorage.setItem(`${response.data.id}`, JSON.stringify(response.data));
+      response.data.fetched = DateTime.local().plus({ hours: 1 }).ts;
+      localStorage.setItem(url, JSON.stringify(response.data));
 
       rocket = response.data;
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
 
   return rocket;
 }
 
-export const getRockets = async () => {
+export const getRockets = async (offset) => {
   try {
-    const response = await axios.get(`${api.devURL}/config/launcher/`);
+    const response = await axios.get(`${api.devURL}/config/launcher/?limit=50&offset=${offset}`);
     return response.data;
   } catch (err) {
     console.error(err)
@@ -54,6 +74,15 @@ export const getRockets = async () => {
 export const getRocketById = async (id) => {
   try {
     const response = await axios.get(`${api.devURL}/config/launcher/${id}`);
+    return response.data;
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getAgencies = async (id) => {
+  try {
+    const response = await axios.get(`${api.devURL}/agencies/?limit=100&featured=true`);
     return response.data;
   } catch (err) {
     console.error(err)
