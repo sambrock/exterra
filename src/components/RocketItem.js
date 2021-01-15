9ac6a7e8b9cdab224af982ffbca7b2d29fd74b09
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2021-01-15 12:56:47 +0000

    Refactored to use tailwind.

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,84 +1,43 @@
-import { useContext } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import getCountryISO2 from 'country-iso-3-to-2';
-import AddCircle from '@material-ui/icons/AddCircle';
 
-import { RocketCompareContext } from '../App';
+const StyledRocketItemDiv = styled.div`
+  border-top: 1px var(--dark-blue-2) solid;
 
-const StyledRocketItem = styled.div`
-  border-bottom: 1px var(--white-opacity-2) solid;
-  display: grid;
-  grid-template-columns: 1fr auto;
-  grid-template-rows: 1fr 1fr;
-  /* margin: 12px 0; */
-  /* padding-bottom: 12px; */
-  padding: 24px;
-
-  &:first-child {
-    border-top: 1px var(--white-opacity-2) solid;
-  }
-
-  .img-container {
-    width: 100%;
-    height: 150px;
-    position: relative;
-    overflow: hidden;
-
-    img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover;
-    }
-  }
-
-  .name {
-    font-weight: 600;
-    font-size: var(--fz-xl);
-    display: flex;
-    align-items: center;
-
-    img {
-      height: .7rem;
-      margin-right: 12px;
-    }
-  }
-
-  .rocket-details {
-    margin-top: 6px;
-    color: var(--white-opacity);
-    display: flex;
-
-    span {
-      margin-right: 24px;
-    }
-  }
-
-  .add {
-    grid-column: 2;
-    grid-row: 1/span 2;
-    ${props => props.theme.mixins.flexCenter};
-
-    .material-icon {
-      height: 35px;
-      width: 35px;
-      color: var(--white-opacity-2);
-      background: none;
-      cursor: pointer;
-    }
+  &:last-child {
+    border-bottom: 1px var(--dark-blue-2) solid;
   }
 `;
 
-export default function RocketItem({ rocket, addCompareId }) {
+export default function RocketItem({ rocket, setCompare, locked }) {
+  const [added, setAdded] = useState(false);
+
+  const handleAddClick = () => {
+    setCompare(arr => [...arr, rocket.id]);
+    setAdded(true);
+  }
+
+  const handleRemoveClick = () => {
+    setCompare(arr => arr.splice(arr.indexOf(rocket.id), 1));
+    setAdded(false);
+  }
+
   return (
-    <StyledRocketItem>
-      <div className="name"><img src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${getCountryISO2(rocket.manufacturer.country_code)}.svg`} alt="" />{rocket.name}</div>
-      <div className="rocket-details">
-        <span>{rocket.manufacturer.name}</span>
-        <span>{rocket.manufacturer.type}</span>
+    <StyledRocketItemDiv className="flex justify-between items-center py-4">
+      <div>
+        <div className="flex items-center"><img className="h-3 sm:h-5 mr-2 sm:mr-4" src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${getCountryISO2(rocket.manufacturer.country_code)}.svg`} alt="" />
+          <div className="text-xxl font-semibold">{rocket.name}</div>
+        </div>
+        <div className="mt-2 text-sm text-opacity">
+          <span className="mr-4">{rocket.manufacturer.name}</span>
+          <span>{rocket.manufacturer.type}</span>
+        </div>
       </div>
-      <div className="add">
-        <AddCircle className="material-icon" onClick={() => addCompareId(rocket.id)} />
-      </div>
-    </StyledRocketItem>
+      {!added ?
+        <button className={`flex items-center text-sm text-opacity ml-3 sm:ml-0 ${locked ? 'hidden' : ''}`} onClick={() => handleAddClick()}><i className="material-icons sm:mr-2">add</i><span className="hidden sm:block">Add to compare</span></button> :
+        <button className="flex items-center text-sm text-opacity ml-3 sm:ml-0" onClick={() => handleRemoveClick()}><i className="material-icons sm:mr-2">close</i><span className="hidden sm:block">Remove</span></button>
+      }
+    </StyledRocketItemDiv>
   )
 }
--- src/components/map/LaunchMarker.js
+++ src/components/map/LaunchMarker.js
@@ -1,66 +1,24 @@
-import styled from 'styled-components';
 import getCountryISO2 from 'country-iso-3-to-2';
 
 import Time from '../Time';
-
-const StyledMarkerContainer = styled.div`
-  display: flex;
-
-  .launch-flag {
-    margin-right: 12px;
-    img {
-      height: .7rem;
-      margin-right: 3px;
-    }
-  }
-`;
-
-const StyledLaunchDetailsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: var(--white-opacity);
-  
-  .launch-name {
-    ${props => props.theme.mixins.heading}
-    font-size: var(--fz-sm);
-  }
-
-  .launch-time-status {
-    display: flex;
-    align-items: center;
-    margin: 3px 0 0; 
-    
-    .launch-time {
-      width: 88px;
-    }
-
-    .launch-status {
-      ${props => props.theme.mixins.status}
-      color: ${props => props.theme.statusColors[props.statusId]};
-      border: 1px solid ${props => props.theme.statusColors[props.statusId]};
-      /* box-shadow: 0 0 3px ${props => props.theme.statusColors[props.statusId]}; */
-      /* text-shadow: 0 0 3px ${props => props.theme.statusColors[props.statusId]}; */
-    }
-  }
-`;
+import { Status } from '../StatusBar';
 
 export default function LaunchMarker({ launch }) {
-  // Fix country code issue
-  if (launch.pad.location.country_code === 'UNK') launch.pad.location.country_code = 'USA';
+  if (launch.pad.location.country_code === 'UNK') launch.pad.location.country_code = 'USA';  // Fix country code issue 
 
   return (
-    <StyledMarkerContainer className="launch-marker">
-      <div className="launch-flag">
-        <img src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${getCountryISO2(launch.pad.location.country_code)}.svg`} alt="" />
+    <div className="flex select-none">
+      <div className="mr-3">
+        <img className="h-4 mt-2" src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${getCountryISO2(launch.pad.location.country_code)}.svg`} alt="" />
       </div>
-      <StyledLaunchDetailsContainer statusId={launch.status.id}>
-        <span className="launch-name">{launch.rocket.configuration.full_name}</span>
-        <span className="launch-service">{launch.launch_service_provider.name}</span>
-        <div className="launch-time-status">
-          <div className="launch-time"><Time launchTime={launch.net} /></div>
-          <div className="launch-status">{launch.status.name}</div>
+      <div className="flex flex-col mt-2" statusId={launch.status.id}>
+        <span className="font-bold text-sm text-white">{launch.rocket.configuration.full_name}</span>
+        <span className="text-opacity">{launch.launch_service_provider.name}</span>
+        <div className="flex items-center mt-1">
+          <div className="pr-4"><Time launchTime={launch.net} /></div>
+          <Status status={launch.status} />
         </div>
-      </StyledLaunchDetailsContainer>
-    </StyledMarkerContainer>
+      </div>
+    </div>
   )
 }
