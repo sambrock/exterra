22e09cf70a2dfaf98214b5cae275a57a821f939a
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2023-05-16 18:12:32 +0100

    added favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,15 @@ export default function Head() {
       <title>EXTERRA</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Space launch web app" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#030917" />
+      <meta name="msapplication-TileColor" content="#030917" />
+      <meta name="theme-color" content="#030917" />
+
+      <link rel="manifest" href="/manifest.json" />
     </>
   );
 }
